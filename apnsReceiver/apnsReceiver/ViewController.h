39bf3c8c8249524074c51c332fd8e06adb410976
maxhu <jhaoheng@gmail.com>
2016-09-20 21:06:07 +0800

    init

---
+++ apnsReceiver/apnsReceiver/ViewController.h
@@ -0,0 +1,43 @@
+//
+//  ViewController.h
+//  apnsReceiver
+//
+//  Created by max on 2016/3/30.
+//  Copyright © 2016年 max hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "slideViewController.h"
+#import "AFNetworking.h"
+#import "http_manager.h"
+#import "cusNaviController.h"
+#import <MessageUI/MFMailComposeViewController.h>
+
+@interface ViewController : UIViewController<MFMailComposeViewControllerDelegate,slideMenuDelegate,httpFinishDelegate>
+{
+    slideViewController *slide;
+    
+    //
+    UILabel *netStatus_label;
+    UIImageView *netStatusImg;
+    
+    UIActivityIndicatorView *indicatorView;
+    UIView *maskView;
+}
+
+@property (nonatomic, retain) NSString *passToken;
+@property (nonatomic, retain) NSDictionary *passUserInfo;
+
+@property (nonatomic, strong) NSString *net_status;
+
+@end
+
+
+
+/*
+ wanted :
+ - 需要把 logView 做得更好一些
+ - 延伸 nslog 的功能
+ */
+
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+![icon](icon_0.png)
+
+此 repo 從 [php_push_ios_gcm](https://github.com/jhaoheng/php_push_ios_gcm) 切割而來
+
+## USE KIT
+
+- [AFNetworking 3.0](http://cocoadocs.org/docsets/AFNetworking/3.0.4/)
+- [slideMenu](https://github.com/jhaoheng/slideMenu_ios)
+
+## TODO
+
+- 手動增加 api 位置
+- 透過 api 傳送 token or 傳送 mail 給用戶
+
+## view
+
+![img](IMG_1.png)
+![img](IMG_2.png)
